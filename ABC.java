11f96f99a643aaf97356b10730f2285ea92815de
Manikanteshwar <manikanteshwar@Manikanteshwars-MacBook-Air.local>
2024-10-13 12:14:31 +0530

    testing commit

---
+++ ABC.java
@@ -0,0 +1,31 @@
+ interface A{
+  void a();
+  void b();
+  void c();
+ void d();
+ }
+abstract class B implements A{
+  public void c(){
+  System.out.println("Iam c ");
+ }
+}
+class M extends B{
+ public void a(){
+  System.out.println("Iam a");
+ }
+ public void b(){
+ System.out.println("iam b");
+ }
+public void d(){
+ System.out.println("I am d");
+ }
+}
+class ABC{
+  public static void main(String args[]){
+   A a1= new M();
+     a1.a();
+     a1.b();
+     a1.c();
+     a1.d();
+   }
+ }
