bee0bb5b6579343b6c848b5985fff1ac2742e3ac
Mani1818-git <manikanteshwarmani57@gmail.com>
2024-11-01 20:34:42 +0530

    prime number.java

---
+++ Prime.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Prime{
+    public static void main(String[]args){
+        int i,m,flag=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Number:");
+        int n=sc.nextInt();
+        m=n/2;
+        if(n==0||n==1){
+            System.out.println(n+"not a prime number");
+        }else{
+            for(i=2;i<=m;i++){
+              if(n%i==0){
+               System.out.println("not a prime number");
+                flag=1;
+                break;
+              }
+            }
+            if(flag==0){
+                System.out.println(n  +  "It is a prime Number");
+            }
+
+        }
+    }
+}
