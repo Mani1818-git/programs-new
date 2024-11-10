c4d194898d5be38c53d379acc7f0dbf3cb4ebdd4
Mani1818-git <manikanteshwarmani57@gmail.com>
2024-11-10 15:57:23 +0530

    sum add

---
+++ Sum.java
@@ -0,0 +1,11 @@
+class Sum{
+    public static void main(String[] args){
+        int[] arr={6,7,5,7,8};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+         System.out.println("sum of array is");
+        }
+         System.out.println(sum);
+    }
+}
